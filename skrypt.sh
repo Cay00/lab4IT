bbd661b3a4faf167c51abc8271932d60d9751f65
Krzysztof <krzysztof.turek007@gmail.com>
2024-05-14 21:51:37 +0200

    utworzy automatycznie 100 plików

--- skrypt.sh
+++ skrypt.sh
@@ -3,4 +3,11 @@
 if [ "$1" = "--date" ]; then
     dzis=$(date +"%Y-%m-%d")
     echo $dzis
+elif [ "$1" = "--logs" ]; then
+    for ((i=1; i<=100; i++)); do
+        nazwa_pliku="log${i}.txt"
+        echo "Nazwa pliku: $nazwa_pliku" > $nazwa_pliku
+        echo "Skrypt: $0" >> $nazwa_pliku
+        echo "Data utworzenia: $(date +"%Y-%m-%d %H:%M:%S")" >> $nazwa_pliku
+    done
 fi
